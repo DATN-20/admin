981dad4efd7994cba96c9a640f60570b744cc095
20120265 <lixhen2002@gmail.com>
2024-05-06 22:26:24 +0700

    feat: init project

---
+++ app/{dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+//make a layout nextjs
+import { Layout } from "antd";
+// make the sidebar for all page here
+import React from "react";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return { children };
+}
